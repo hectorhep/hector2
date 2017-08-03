25dd3c77cb57530f92dd3ea974223d8715d4386a
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-08-03 11:09:21 +0200

    First attempt to generate a ROOT dictionary of all objects

---
+++ Hector/IO/Linkdef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#include "Hector/Propagator/Particle.h"
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class CLHEP::HepVector+;
+#pragma link C++ class Hector::StateVector+;
+#pragma link C++ class Hector::Particle+;
+
+#endif
