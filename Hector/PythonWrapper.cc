2722ef09b66c96d6a02717cdf9319e79d7a7e2b5
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-11-06 10:02:59 +0100

    First working version with parameters getters

--- Hector/PythonWrapper.cc
+++ Hector/PythonWrapper.cc
@@ -1,6 +1,7 @@
 #include <boost/python.hpp>
 #include <boost/python/overloads.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/python/return_internal_reference.hpp>
 
 #include <CLHEP/Vector/LorentzVector.h>
 
@@ -13,57 +14,58 @@
 #include "Beamline/Beamline.h"
 #include "IO/MADXParser.h"
 
-BOOST_PYTHON_MODULE( libpyhector )
+BOOST_PYTHON_MODULE( pyhector )
 {
-  boost::python::class_<CLHEP::HepLorentzVector>( "lorentzVector" )
+  namespace py = boost::python;
+/*  py::class_<CLHEP::HepLorentzVector>( "lorentzVector" )
     .def( "px", &CLHEP::HepLorentzVector::px )
     .def( "py", &CLHEP::HepLorentzVector::py )
     .def( "pz", &CLHEP::HepLorentzVector::pz )
     .def( "e", &CLHEP::HepLorentzVector::e )
   ;
 
-  boost::python::enum_<Hector::ExceptionType>( "exceptionType" )
+  py::enum_<Hector::ExceptionType>( "exceptionType" )
     .value( "undefined", Hector::ExceptionType::Undefined )
     .value( "info", Hector::ExceptionType::Info )
     .value( "justWarning", Hector::ExceptionType::JustWarning )
     .value( "fatal", Hector::ExceptionType::Fatal )
-  ;
+  ;*/
 
-  boost::python::class_<Hector::Parameters, std::shared_ptr<Hector::Parameters> >( "parameters", boost::python::init<>() )
+  py::class_<Hector::Parameters, std::shared_ptr<Hector::Parameters>, boost::noncopyable>( "parameters", py::no_init )
     .def( "get", &Hector::Parameters::get ).staticmethod( "get" )
-    .def( "beamEnergy", &Hector::Parameters::beamEnergy )
-    .def( "beamParticlesMass", &Hector::Parameters::beamParticlesMass )
-    .def( "crossingAngleX", &Hector::Parameters::crossingAngleX )
-    .def( "crossingAngleY", &Hector::Parameters::crossingAngleX )
-    .def( "beamParticlesCharge", &Hector::Parameters::beamParticlesCharge )
-    .def( "loggingThreshold", &Hector::Parameters::loggingThreshold )
-    .def( "useRelativeEnergy", &Hector::Parameters::useRelativeEnergy )
-    .def( "correctBeamlineOverlaps", &Hector::Parameters::correctBeamlineOverlaps )
-    .def( "computeApertureAcceptance", &Hector::Parameters::computeApertureAcceptance )
-    .def( "enableKickers", &Hector::Parameters::enableKickers )
-    .def( "enableDipoles", &Hector::Parameters::enableDipoles )
+    .def( "beamEnergy", &Hector::Parameters::beamEnergy, py::return_value_policy<py::return_by_value>() )
+    .def( "beamParticlesMass", &Hector::Parameters::beamParticlesMass, py::return_value_policy<py::return_by_value>() )
+    .def( "crossingAngleX", &Hector::Parameters::crossingAngleX, py::return_value_policy<py::return_by_value>() )
+    .def( "crossingAngleY", &Hector::Parameters::crossingAngleX, py::return_value_policy<py::return_by_value>() )
+    .def( "beamParticlesCharge", &Hector::Parameters::beamParticlesCharge, py::return_value_policy<py::return_by_value>() )
+    .def( "loggingThreshold", &Hector::Parameters::loggingThreshold, py::return_value_policy<py::return_by_value>() )
+    .def( "useRelativeEnergy", &Hector::Parameters::useRelativeEnergy, py::return_value_policy<py::return_by_value>() )
+    .def( "correctBeamlineOverlaps", &Hector::Parameters::correctBeamlineOverlaps, py::return_value_policy<py::return_by_value>() )
+    .def( "computeApertureAcceptance", &Hector::Parameters::computeApertureAcceptance, py::return_value_policy<py::return_by_value>() )
+    .def( "enableKickers", &Hector::Parameters::enableKickers, py::return_value_policy<py::return_by_value>() )
+    .def( "enableDipoles", &Hector::Parameters::enableDipoles, py::return_value_policy<py::return_by_value>() )
   ;
 
-  boost::python::class_<Hector::Particle>( "particle" )
+  py::class_<Hector::Particle>( "particle" )
     .def( "pdgId", &Hector::Particle::pdgId )
     .def( "momentumAt", &Hector::Particle::momentumAt )
     .def( "stateVectorAt", &Hector::Particle::stateVectorAt )
   ;
 
-  boost::python::class_<Hector::StateVector>( "stateVector" )
+  py::class_<Hector::StateVector>( "stateVector" )
     .def( "energy", &Hector::StateVector::energy )
     .def( "xi", &Hector::StateVector::xi )
     .def( "momentum", &Hector::StateVector::momentum )
   ;
 
-  boost::python::class_<Hector::Beamline>( "beamline" )
+/*  py::class_<Hector::Beamline>( "beamline" )
     //.def( "sequencedBeamline", &Hector::Beamline::sequencedBeamline ).staticmethod( "sequencedBeamline" )
     .def( "clear", &Hector::Beamline::clear )
     .def( "addElement", &Hector::Beamline::addElement )
   ;
 
-  boost::python::class_<Hector::Parser::MADX>( "madXparser", boost::python::init<const char*,const char*,int,boost::python::optional<float> >() )
+  py::class_<Hector::Parser::MADX>( "madXparser", py::init<const char*,const char*,int,py::optional<float> >() )
     .def( "beamline", &Hector::Parser::MADX::beamline )
     .def( "romanPots", &Hector::Parser::MADX::romanPots )
-  ;
+  ;*/
 }
