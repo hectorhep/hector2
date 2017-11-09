53109bfa8f93c014096347668f30d5a95b046fad
Laurent Forthomme <laurent.forthomme@cern.ch>
2017-11-09 11:04:05 +0100

    Corrected initialiser for element_name field

--- Hector/IO/HBLFileStructures.h
+++ Hector/IO/HBLFileStructures.h
@@ -17,15 +17,13 @@ namespace Hector
     struct HBLElement
     {
       HBLElement() :
-        element_type( -1 ), element_name( "" ),
-        element_s( 0. ), element_length( 0. ),
+        element_type( -1 ), element_name{ 0 }, element_s( 0. ), element_length( 0. ),
         element_magnetic_strength( 0. ),
         aperture_type( -1 ),
         aperture_p1( 0. ), aperture_p2( 0. ), aperture_p3( 0. ), aperture_p4( 0. ),
         aperture_x( 0. ), aperture_y( 0. ) {}
       HBLElement( const HBLElement& rhs ) :
-        element_type( rhs.element_type ),
-        element_s( rhs.element_s ), element_length( rhs.element_length ),
+        element_type( rhs.element_type ), element_s( rhs.element_s ), element_length( rhs.element_length ),
         element_magnetic_strength( rhs.element_magnetic_strength ),
         aperture_type( rhs.aperture_type ),
         aperture_p1( rhs.aperture_p1 ), aperture_p2( rhs.aperture_p1 ), aperture_p3( rhs.aperture_p3 ), aperture_p4( rhs.aperture_p4 ),
